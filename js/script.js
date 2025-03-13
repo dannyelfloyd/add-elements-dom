c75cdfca19c7efa1a7eb9e5cfa45f1bac1e81f1a
dannyelfloyd <dannyelfloyd@gmail.com>
2025-03-13 09:09:51 +0100

    Challege completado. Funciona

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
  // Aquí tu código
+ /*
+ const pulse = document.getElementById('agregar');
+ const addPrompt = document.getElementById('lista').appendChild(document.createElement('li'));
+
+ pulse.addEventListener('click', function () {
+    addPrompt.insertAdjacentHTML('afterbegin',prompt('Introduccir dato:'));
+  });
+*/
+  const buttonAdd = document.getElementById('agregar');
+  
+  buttonAdd.addEventListener('click', addFunction);
+  
+  function addFunction () {
+    const parent = document.getElementById('lista');
+    const child = document.createElement('li');
+    parent.appendChild(child).insertAdjacentHTML('afterbegin',prompt('Introduccir dato:'));
+  }
